a225a0d0f6057aad139543df3687be2f46546e2c
silentdevnull <test@test>
2025-04-19 19:32:37 -0700

    New theme almost complete

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,7 +7,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
-
+vim.opt.timeoutlen = 500
 -- Remapping
 
 vim.g.mapleader = ' '
@@ -15,6 +15,7 @@ vim.g.maplocalleader = ' '
 
 -- keymap
 
-vim.keymap.set("n","<leader>fo",vim.cmd.Ex)
-vim.keymap.set("i","<C-w>","<Esc>:w<Enter>")
+-- vim.keymap.set("n", "<leader>fo", vim.cmd.Ex)
+-- vim.keymap.set("i", "<C-w>", "<Esc>:w<Enter>")
 
+vim.keymap.set("i", "jl", "<Esc>", { noremap = true })
