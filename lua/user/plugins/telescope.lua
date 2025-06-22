ed7c247b715ccaf589210f90a1481ac265a740f2
silentdevnull <86895479+silentdevnull@users.noreply.github.com>
2025-06-22 15:11:52 -0700

    Working on telescope

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,111 +1,97 @@
--- In your plugins.lua or similar file
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- For file icons
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-      'nvim-telescope/telescope-file-browser.nvim',
-    },
-    config = function()
-      local telescope = require('telescope')
-      local actions = require('telescope.actions')
+  "nvim-telescope/telescope.nvim",
+  -- No tag, lazy.nvim will get a recent version
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+    local actions = require("telescope.actions") -- Require actions here for general use
 
-      telescope.setup({
-        defaults = {
-          -- Your general Telescope defaults (can be omitted if already global)
-          -- ... (e.g., prompt_prefix, selection_caret, etc. from previous answer)
+    -- General Telescope keymaps
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+
+    telescope.setup({
+      defaults = {
+        -- It's good practice to ensure Esc in insert mode goes to normal mode
+        -- and Esc in normal mode closes the picker.
+        mappings = {
+          i = {
+            ["<C-c>"] = actions.close, -- Close picker
+            -- You might want to add <C-j>, <C-k> for navigation in insert mode
+            -- ["<C-j>"] = actions.move_selection_next,
+            -- ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = {
+            ["<Esc>"] = actions.close, -- Close picker
+            ["q"] = actions.close,     -- Close picker
+          },
+        },
+      },
+      pickers = {
+        -- Configure pickers if needed
+      },
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+          -- path = "%:p:h", -- Consider setting a default path if desired
+          -- hidden = true,
           mappings = {
-            i = {
-              -- ... common Telescope mappings
+            ["n"] = { -- Mappings for file_browser NORMAL mode
+              ["n"] = function(prompt_bufnr)
+                -- Use a distinct local variable for actions if preferred,
+                -- or rely on the 'actions' from the outer scope.
+                local fb_actions = require("telescope.actions")
+                local current_picker =
+                    require("telescope.actions.state").get_current_picker(
+                      prompt_bufnr
+                    )
+                local current_dir = current_picker.cwd
+                if current_dir == nil or current_dir == "" then
+                  current_dir = vim.fn.getcwd()
+                end
+
+                fb_actions.close(prompt_bufnr) -- Close telescope
+
+                vim.ui.input({ prompt = "New file name: " }, function(
+                    filename
+                )
+                  if filename == nil or filename == "" then
+                    vim.notify("File creation cancelled.", vim.log.levels.INFO)
+                    return
+                  end
+                  local new_file_path = vim.fs.joinpath(current_dir, filename)
+                  if vim.fn.filereadable(new_file_path) == 1 then
+                    vim.notify(
+                      "File already exists: " .. new_file_path,
+                      vim.log.levels.WARN
+                    )
+                    vim.cmd("edit " .. vim.fn.fnameescape(new_file_path))
+                  else
+                    vim.cmd("edit " .. vim.fn.fnameescape(new_file_path))
+                    vim.notify(
+                      "Opened new file: " .. new_file_path,
+                      vim.log.levels.INFO
+                    )
+                  end
+                end)
+              end,
+              -- Add other file_browser normal mode mappings here if needed
             },
-            n = {
-              -- ... common Telescope mappings
+            ["i"] = {
+              -- Add file_browser insert mode mappings here if needed
             },
           },
         },
-        pickers = {
-          -- Your specific picker configurations (e.g., find_files)
-        },
-        extensions = {
-          file_browser = {
-            -- Key for a more "tree-like" experience:
-            theme = "dropdown",  -- "ivy" or "dropdown" can work well for this
-            hijack_netrw = true,
-            hidden = true,       -- Show hidden files by default
-            grouped = true,      -- Group directories and files
-            files_on_top = true, -- Show files above directories
-            -- This function allows you to customize commands for file_browser
-            -- This is where you can add things like "create file/dir" or "delete"
-            -- for the file_browser
-            on_attach = function(bufnr)
-              -- Common mappings for file_browser when it's open
-              vim.keymap.set('n', 'h', actions.goto_parent_dir, { buffer = bufnr })
-              vim.keymap.set('n', 'l', actions.change_working_dir, { buffer = bufnr })
-              vim.keymap.set('n', '.', actions.toggle_hidden, { buffer = bufnr, desc = "Toggle hidden files" })
-              vim.keymap.set('n', '-', actions.goto_parent_dir, { buffer = bufnr })
-              vim.keymap.set('n', 'q', actions.close, { buffer = bufnr })
-              vim.keymap.set('i', '<C-c>', actions.close, { buffer = bufnr })
+      },
+    })
 
-              -- Custom actions for file_browser
-              vim.keymap.set('n', 'a', function()
-                -- Example: Create a new file
-                vim.fn.input("New file name: ")
-                -- You'd need to expand this to actually create the file
-                -- based on the current directory.
-                -- For a real file system operation, you'd call out to Lua's io.open or vim.fn.mkdir/writefile
-              end, { buffer = bufnr, desc = "Create new file" })
-
-              vim.keymap.set('n', 'A', function()
-                -- Example: Create a new directory
-                vim.fn.input("New directory name: ")
-              end, { buffer = bufnr, desc = "Create new directory" })
-
-              -- To delete files, you'd typically select them and then have a delete action
-              -- e.g., 'd' to delete selected file/directory after confirmation
-              -- This requires a bit more logic than a simple keymap.
-            end,
-          },
-        },
-      })
-
-      -- Load extensions
-      telescope.load_extension('fzf')
-      telescope.load_extension('file_browser')
-
-      -- Keymaps for Telescope File Browser
-      local keymap = vim.keymap.set
-      local opts = { noremap = true, silent = true }
-
-      -- Open file browser in current directory
-      keymap("n", "<leader>fe", function()
-        telescope.extensions.file_browser.file_browser({
-          path = vim.fn.expand("%:p:h"), -- Start in current file's directory
-          -- Set to 'true' to jump to selected file after opening file browser
-          -- Also, you might want a different 'initial_mode' ('normal' or 'insert')
-          initial_mode = "normal",
-        })
-      end, opts)
-
-      -- Open file browser from root of project
-      keymap("n", "<leader>fr", function()
-        telescope.extensions.file_browser.file_browser({
-          path = vim.fn.getcwd(), -- Start in current working directory
-          initial_mode = "normal",
-        })
-      end, opts)
-
-      -- Open file browser, specifically targeting a directory
-      keymap("n", "<leader>fd", function()
-        telescope.extensions.file_browser.file_browser({
-          path = "%:p:h",
-          select_buffer = true -- Highlight current file if it's in the list
-        })
-      end, opts)
-
-      -- You can also bind directly to the command:
-      -- keymap("n", "<leader>fb", "<cmd>Telescope file_browser<CR>", opts)
-    end,
-  },
+    -- IMPORTANT: Load extensions AFTER telescope.setup() has run.
+    -- This ensures that any default configurations for the extension
+    -- (like the mappings we defined above) are registered with Telescope
+    -- before the extension itself is fully loaded.
+    pcall(telescope.load_extension, "file_browser")
+  end,
 }
